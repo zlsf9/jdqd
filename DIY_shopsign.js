e3736f1e34e6a83a6a8384d4d63ce3220f0232e5
cui521 <46080261+cui521@users.noreply.github.com>
2021-03-26 00:40:27 +0800

    Update DIY_shopsign.js

--- DIY_shopsign.js
+++ DIY_shopsign.js
@@ -1,6 +1,6 @@
 /*
 店铺签到，各类店铺签到，有新的店铺直接添加token即可
-0 0 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js
+cron "0 0 * * * "https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js
 */
 
 const $ = new Env('跑路·······');
